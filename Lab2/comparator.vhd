c1697889f78bf1eaf9f5c56278d42d42745605d4
ghb130 <graehmeblair2019@u.northwestern.edu>
2017-10-08 15:01:16 -0500

    Changed Comparator

--- Lab2/comparator.vhd
+++ Lab2/comparator.vhd
@@ -1,20 +1,18 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use WORK.divider_const.all;
 --Additional standard or custom libraries go here
 entity comparator is
-generic(
-DATA_WIDTH : natural := 4
-);                                      -- Should make smallerwidth + 1
 port(
 --Inputs
-    DINL : in std_logic_vector (DATA_WIDTH downto 0); -- current portion of dividend
-    DINR : in std_logic_vector (DATA_WIDTH - 1 downto 0); -- divisor
+    DINL : in std_logic_vector (DIVISOR_WIDTH downto 0); -- current portion of dividend
+    DINR : in std_logic_vector (DIVISOR_WIDTH - 1 downto 0); -- divisor
 --Outputs
-    DOUT : out std_logic_vector (DATA_WIDTH - 1 downto 0);  -- This should probably be DATA_WIDTH downto 0
+    DOUT : out std_logic_vector (DIVISOR_WIDTH - 1 downto 0);  -- This should probably be DATA_WIDTH downto 0
     isGreaterEq : out std_logic;
     overflow: out std_logic
-    );
+  );
 end entity comparator;
 
 architecture behavioral of comparator is
@@ -28,7 +26,7 @@ architecture behavioral of comparator is
         DINL_u := unsigned(DINL);
         DINR_u := unsigned(DINR);
         if DINL_u < DINR_u then
-          DOUT <=  std_logic_vector(resize(DINL_u, DATA_WIDTH-1));
+          DOUT <=  std_logic_vector(resize(DINL_u, DIVISOR_WIDTH));
           isGreaterEq <= '0';
         else
           -- MIGHT NEED TO CHECK FOR OVERFLOW
@@ -36,7 +34,7 @@ architecture behavioral of comparator is
             overflow <= '1';
             DINR_u := to_unsigned(1, DATA_WIDTH-1);
           end if;
-          DOUT <= std_logic_vector(resize(DINL_u-('0' & DINR_u), DATA_WIDTH-1));
+          DOUT <= std_logic_vector(resize(DINL_u-('0' & DINR_u), DIVISOR_WIDTH));
           isGreaterEq <= '1';
         end if;
     end process compare;
