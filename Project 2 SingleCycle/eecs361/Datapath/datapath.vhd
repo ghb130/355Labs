a9c5ce584bcd921fbe6511e35a3a06bfeb9007bf
ghb130 <graehmeblair2019@u.northwestern.edu>
2017-11-08 19:16:20 -0600

    Initial Datapath commit

---
+++ Project 2 SingleCycle/eecs361/Datapath/datapath.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.eecs361_gates.all;
+use work.eecs361.all;
+
+entity datapath is
+  port (
+  );
+end entity;
+
+architecture structural of datapath is
+begin
+  
+end architecture;
