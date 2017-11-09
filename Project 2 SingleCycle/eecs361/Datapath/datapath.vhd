8cc4c11425d5003bf4b55d4a346a893e4223f062
ghb130 <graehmeblair2019@u.northwestern.edu>
2017-11-08 19:35:28 -0600

    Signals Datapath

--- Project 2 SingleCycle/eecs361/Datapath/datapath.vhd
+++ Project 2 SingleCycle/eecs361/Datapath/datapath.vhd
@@ -5,10 +5,25 @@ use work.eecs361.all;
 
 entity datapath is
   port (
+    RegDst : in std_logic;
+    Regwr : in std_logic;
+    Branch : in std_logic;
+    ExtOp : in std_logic;
+    ALUSrc : in std_logic;
+    ALUCtrl : in std_logic_vector (3 downto 0);
+    MemWr : in std_logic;
+    MemtoReg : in std_logic;
+    BranchSel : in std_logic_vector (2 downto 0);
+    clk : in std_logic
   );
 end entity;
 
 architecture structural of datapath is
+  signal Rd, Rt, Rs : std_logic_vector(4 downto 0);
+  signal imm : std_logic_vector(15 downto 0);
+  signal busa, busb, busw : std_logic_vector(31 downto 0);
+  signal extend, ALUsrcMux, ALUout, dataMemOut : std_logic_vector(31 downto 0);
+  signal zero : std_logic;
+
 begin
-  
 end architecture;
