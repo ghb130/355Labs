d52c889e3af80748262a6b0be8f1ee9c62fa951c
drewhasse <drew091996@gmail.com>
2017-11-08 19:50:09 -0600

    deleted test

--- Project 2 SingleCycle/eecs361/Datapath/datapath.vhd
+++ Project 2 SingleCycle/eecs361/Datapath/datapath.vhd
@@ -25,7 +25,6 @@ architecture structural of datapath is
   signal busa, busb, busw : std_logic_vector(31 downto 0);
   signal extend, ALUsrcMux, ALUout, dataMemOut : std_logic_vector(31 downto 0);
   signal zero : std_logic;
-  signal test : std_logic;
 
 begin
 
